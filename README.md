d92ab17fd8ad6ace7206b0a3810a5629abb8845c
Nevin Herring <nevin.herring@gmail.com>
2018-10-04 19:52:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Miner's Site
+#see facebook for picture credits
+#copyright 2018 Miner's theatre foundation
---
+++ app.js
@@ -0,0 +1,42 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  delay: 500,
+  duration: 2000,
+  origin: 'bottom'
+});
+sr.reveal('.title', {
+  duration: 2000,
+  origin: 'top',
+  distance: '500px'
+});
+sr.reveal('.about-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('.about-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px'
+});
+sr.reveal('.about-btn', {
+  duration: 2000,
+  delay: 2000,
+  origin: 'bottom'
+});
+sr.reveal('#testimonial div', {
+  duration: 2000,
+  origin: 'bottom',
+});
+sr.reveal('.important-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px',
+  viewFactor: 0.2
+});
+sr.reveal('.important-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px',
+  viewFactor: 0.2
+});
